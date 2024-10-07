be755c1c871567d79b561a31aae9690879e8d3cc
isromero <ismaromeroortega@gmail.com>
2024-10-07 19:14:15 +0200

    Add public portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export"
-};
+let nextConfig = {};
+
+if (process.env.NODE_ENV !== "development") {
+  nextConfig = {
+    output: "export",
+  };
+}
 
 export default nextConfig;
- 
