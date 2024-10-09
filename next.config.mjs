2d6fd76549faee0ffcd62ca6199ff9ec0036efef
isromero <ismaromeroortega@gmail.com>
2024-10-09 19:32:07 +0200

    Fix fonts

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "fonts/[name].[hash].[ext]",
+          publicPath: "/_next/static/",
+          outputPath: "static/",
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
